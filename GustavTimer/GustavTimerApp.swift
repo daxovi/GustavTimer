777599717e82f81c3eb430b7e327a0ab80119f51
Dalibor Janeček <dalibor.janecek@gmail.com>
2023-04-19 18:46:29 +0200

    Initial Commit

---
+++ GustavTimer/GustavTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GustavTimerApp.swift
+//  GustavTimer
+//
+//  Created by Dalibor Janeček on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GustavTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
