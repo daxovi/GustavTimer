96b61a980398cb34514f98884b9386959dcd3081
Dalibor Janeček <dalibor@daliborjanecek.cz>
2026-02-20 15:40:26 +0100

    TelemetryDeck integration

--- GustavTimer/GustavTimerApp.swift
+++ GustavTimer/GustavTimerApp.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 import SwiftData
 import GustavUI
+import TelemetryDeck
 
 @main
 struct GustavTimerApp: App {
     
     init() {
         GustavDesign.registerFonts()
+        
+        let config = TelemetryDeck.Config(appID: "E2CAE467-D3DB-4D72-9B05-1F026C60E7B7")
+        TelemetryDeck.initialize(config: config)
     }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
